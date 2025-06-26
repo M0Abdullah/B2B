c82a9c01a95e210ff4df98a9c01cde73a8741810
@Muhmmad_Abdullah784 <163482773+M0Abdullah@users.noreply.github.com>
2025-06-26 13:16:41 +0500

    Update axiosInstance.ts

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,10 +1,26 @@
+import loginStore from "@/store/page";
 import axios from "axios";
-
 const axiosInstance = axios.create({
   baseURL: "https://smab.pythonanywhere.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+// Add a response interceptor to catch 401 errors
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Clear tokens (optional)
+      localStorage.removeItem("access");
+      localStorage.removeItem("refresh");
+      loginStore.reset()
+      // Redirect to login page
+      window.location.href = "/login"; 
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
