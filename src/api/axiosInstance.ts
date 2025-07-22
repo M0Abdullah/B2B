d7b2d8ed33a80bac6b745cb99b4a7658c1855c94
@Muhmmad_Abdullah784 <163482773+M0Abdullah@users.noreply.github.com>
2025-07-22 16:35:10 +0500

    Update axiosInstance.ts

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -7,16 +7,13 @@ const axiosInstance = axios.create({
   },
 });
 
-// Add a response interceptor to catch 401 errors
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      // Clear tokens (optional)
       localStorage.removeItem("access");
       localStorage.removeItem("refresh");
       loginStore.reset()
-      // Redirect to login page
       window.location.href = "/login"; 
     }
     return Promise.reject(error);
